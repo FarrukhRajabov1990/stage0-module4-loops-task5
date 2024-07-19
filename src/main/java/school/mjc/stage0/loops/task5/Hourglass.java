1962539d7f523a26ff2a2d9e161e508e51267541
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-07-19 23:38:34 +0500

    Initial commit

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,5 +3,13 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
 
+        for (int i= 0; i < height; i++) {
+            for (int j=0; j<height; j++) {
+                if (i == 0 || i == height-1 || i == j || i + j == height){
+                    System.out.println(8);
+                }
+            }
+            System.out.println();
+        }
     }
 }
